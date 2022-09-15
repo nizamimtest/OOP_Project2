82cac9dae8ab47fb3f2b449b0fe537939952f930
nizamimtest <nizamimtest@gmail.com>
2022-09-15 20:29:41 +0400

    OOP Project2 has been created

---
+++ src/day033/Sekil.java
@@ -0,0 +1,11 @@
+package day033;
+
+public abstract class Sekil {
+    public void bilgiGoster(){
+        System.out.println("Geometrik sekil olusturuldu");
+    }
+    abstract double alanHesapla();
+    abstract double cevreHesapla();
+    abstract void ciz();
+
+}
